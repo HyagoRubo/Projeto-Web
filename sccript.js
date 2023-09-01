9e161275050bdc7e1b4c7db6c09b5f086e3c96db
HyagoRubo <124307564+HyagoRubo@users.noreply.github.com>
2023-08-31 22:42:07 -0300

    Add files via upload

---
+++ sccript.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const buyButton = document.getElementById("buy-button");
+
+    buyButton.addEventListener("click", function() {
+        alert("Thank you for your purchase!");
+    });
+});
